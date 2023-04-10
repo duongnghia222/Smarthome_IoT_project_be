556d27903baa14576dcf75a3f7d7cca3b7d8d0de
git <duongnghia222>
2023-04-10 23:04:28 +0700

    up code be

---
+++ src/cloud/mongoDB.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('Connected to Mongo DB')
+}).catch(err => console.log(err))
+
+module.exports = mongoose;
